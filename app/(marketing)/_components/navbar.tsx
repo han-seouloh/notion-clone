66191bbc155c793c382b0236b18d57892b18b551
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-11-01 16:31:44 -0600

    Add convex/clerk auth

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
 
   return(
     <div className={
-      cn('z-50 bg-background fixed top-0 flex items-center w-full p-6', scrolled && 'border-b shadow-sm')
+      cn('z-50 bg-background dark:bg-[#1f1f1f] fixed top-0 flex items-center w-full p-6', scrolled && 'border-b shadow-sm')
     }>
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://intense-anchovy-3.clerk.accounts.dev',
+      applicationID: 'convex',
+    }
+  ],
+}
