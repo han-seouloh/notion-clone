e1af37f8200a6ce83981603306af28e3d32708b6
HanSeoulOh <112726708+han-seouloh@users.noreply.github.com>
2023-11-04 19:39:39 -0600

    Added nesting functionality to sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,18 +5,18 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useRef, useState, useEffect } from 'react';
 import { useMediaQuery } from "usehooks-ts";
 
-import { useQuery, useMutation } from 'convex/react';
+import { useMutation } from 'convex/react';
 import { api } from "@/convex/_generated/api";
 import { toast } from 'sonner';
 
 import { cn } from "@/lib/utils";
 import { UserItem } from "./user-item";
 import { Item } from "./item";
+import { DocumentList } from "./document-list";
 
 export const Navigation = () => {
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
-  const documents = useQuery(api.documents.get);
   const create = useMutation(api.documents.create);
 
   const isResizingRef = useRef(false);
@@ -144,7 +144,7 @@ export const Navigation = () => {
           />
         </div>
         <div className="mt-4">
-          {documents?.map((document) => (<p key={document._id}>{document.title}</p>))}
+          <DocumentList />
         </div>
         <div 
           onMouseDown={handleMouseDown}
